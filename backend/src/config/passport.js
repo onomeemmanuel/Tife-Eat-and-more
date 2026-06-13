e531791b2d83667f85c02248ecec04c982b4a717
Your Name <your.onomee945@.com>
2026-06-13 19:26:54 +0100

    Fix backend passport import casing for Render Linux

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const User = require('../models/User.model');
+const User = require('../models/user.model');
 
 console.log('Google OAuth callback URL:', process.env.GOOGLE_CALLBACK_URL);
 console.log('Google OAuth client ID present:', Boolean(process.env.GOOGLE_CLIENT_ID));
