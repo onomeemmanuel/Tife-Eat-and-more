9fc693888734b2a90b7428172e7506842c359c71
Your Name <your.onomee945@.com>
2026-06-13 19:41:16 +0100

    Fix backend user model import casing for Render deployment

--- backend/src/middleware/middleware.js
+++ backend/src/middleware/middleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User.model');
+const User = require('../models/user.model');
 
 const protect = async (req, res, next) => {
   let token;
